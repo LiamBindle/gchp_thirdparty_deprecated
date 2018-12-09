56fc6f8dce05ef5136c5a9d7034576d8ad5be73f
Liam Bindle <liam.bindle@gmail.com>
2018-12-09 17:09:49 -0400

    updated build script

--- build.sh
+++ build.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
+set -e
+set -x
 
-source environment.rc
+# Directories
+export ESMF_DIR=$(pwd)/ESMF
+export ESMADIR=$(pwd)/Shared
+export FV_DIR=$(pwd)/FVdycoreCubed_GridComp
+export ESMF_INSTALL_PREFIX=$(pwd)/esmf_install
+
+export ESMF_BOPT=O
+export ESMF_COMPILER=intel
+export ESMF_COMM=openmpi
+export ESMF_OS=Linux
+
 export ESMF_INSTALL_LIBDIR=lib
 export ESMF_INSTALL_BINDIR=bin
 export ESMF_INSTALL_DOCDIR=doc
 export ESMF_INSTALL_HEADERDIR=include
 export ESMF_INSTALL_MODDIR=include
 
-if [ ! -f install/esmf.install ]; then
-	cd $ESMF_DIR && make --no-print-directory && make --no-print-directory install && touch install/esmf.install && cd ..
+# Build ESMF
+if [ ! -f esmf.install ]; then
+	cd $ESMF_DIR
+	make --no-print-directory
+	make --no-print-directory install 
+	touch esmf.install 
+	cd ..
 fi
 
-# Build and install FVDycore
-if [ ! -f install/fvdycore.install ]; then
-	cd $FV_DIR && make --no-print-directory install && touch install fvdycore.install && cd ..
+# Build MAPL
+if [ ! -f mapl.install ]; then
+	cd $ESMADIR 
+	make --no-print-directory install 
+	touch mapl.install 
+	cd ..
 fi
+
